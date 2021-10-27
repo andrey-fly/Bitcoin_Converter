cab08f46db49b6414bfe38f0f878d2d54780d24a
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-10-27 15:21:04 +0300

    Implemented interaction with API(coinapi.io)

--- Bitcoin/Controller/ViewController.swift
+++ Bitcoin/Controller/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
@@ -20,7 +12,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
-        
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
