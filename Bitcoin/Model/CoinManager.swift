8cce6dd195ff447aabafe23528b4d86929a86a35
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-10-27 13:34:52 +0300

    Add UIPicker logic

--- Bitcoin/Model/CoinManager.swift
+++ Bitcoin/Model/CoinManager.swift
@@ -11,9 +11,11 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = ProcessInfo.processInfo.environment["API_KEY"]!
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    func getCoinPrice(for currency: String) {
+        
+    }
 }
